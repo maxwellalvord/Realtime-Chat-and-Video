85d95ab38b7e8d49a72d99c376f7a259a2053588
Maxwell Alvord <maxwellalvord@gmail.com>
2023-09-06 19:10:10 -0700

    Update: finish out authentication with user button component from clerk.

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,9 @@
-
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <div className="">
-      This is a protected route
+    <div>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
