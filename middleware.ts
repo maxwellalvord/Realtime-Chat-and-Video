ce67af24402fea8b3a9330ace745011016a33ba0
Maxwell Alvord <maxwellalvord@gmail.com>
2023-09-14 07:32:11 -0700

    WIP: install uploadthing package to build out image upload component

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["/api/uploadthing"] // uploadthing still goes through authentication but need to be declare a public route to prevent errors.
+});
  
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
